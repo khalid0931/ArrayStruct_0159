1a917b953942b1aa1e065dac40f15dfadfa0f953
khalid0931 <mutiarif53@gmail.com>
2026-04-06 15:31:49 +0700

    membuat int main

--- StructNew.cpp
+++ StructNew.cpp
@@ -7,4 +7,18 @@ struct Orang
     string alamat;
     int umur;
 };
+int main() {
+    Orang mhs;
+    cout << "Nama : ";
+    cin >> mhs.nama;
+    cout << "Alamat : ";
+    cin >> mhs.alamat;
+    cout << "Umur : ";
+    cin >> mhs.umur;
+    cout << endl;
 
+    // tampilkan
+    cout << " Nama : " << mhs.nama << endl;
+    cout << " Alamat : " << mhs.alamat << endl;
+    cout << " Umur : " << mhs.umur << endl;
+}
