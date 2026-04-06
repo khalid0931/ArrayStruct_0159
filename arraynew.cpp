4d9af344339772f861b41daa308f86e0f618ca0e
khalid0931 <mutiarif53@gmail.com>
2026-04-06 15:05:47 +0700

    mengisi data satu per satu

--- arraynew.cpp
+++ arraynew.cpp
@@ -15,4 +15,20 @@ int main() {
     aku[1] = 200;
     cout << endl;
     cout << "Data pada index 1 = " << aku[1] << endl;
+
+     // Mengisi Satu per Satu
+    cout << "Data index 0 = ";
+    cin >> kamu[0];
+    
+    cout << "Data index 1 = ";
+    cin >> kamu[1];
+    
+    cout << "Data index 2 = ";
+    cin >> kamu[2];
+    
+    cout << "Data index 3 = ";
+    cin >> kamu[3];
+    
+    cout << "Data index 4 = ";
+    cin >> kamu[4]; 
 }
