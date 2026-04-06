fa0e9ed84f63fb41749d8346ef4bcbcf0793b494
khalid0931 <mutiarif53@gmail.com>
2026-04-06 15:11:31 +0700

    menambahkan endline

--- arraynew.cpp
+++ arraynew.cpp
@@ -39,4 +39,5 @@ int main() {
     cout << "Data Ketiga = " << kamu[2] << endl;
     cout << "Data Keempat = " << kamu[3] << endl;
     cout << "Data Kelima = " << kamu[4] << endl;
+    cout << endl;
 }
